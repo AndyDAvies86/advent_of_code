54e8ef692d87ba0f7f8d06582a03f153937f3f8b
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2024-06-09 22:35:01 +0100

    2019 day 11 wokring intcode

--- 2019/Day11/solution.py
+++ 2019/Day11/solution.py
@@ -129,18 +129,112 @@ def parse(inputlist):
     return[int(x) for x in inputlist]
 
 
+def get_to_output(intcode):
+    while len(intcode.mem)<2:
+        if intcode.program[intcode.pos] == 99:
+            return
+        intcode.oprun()
+    return
+
 #%%        
 
 def part1(inputlist):
-    program = parse(startlist)
-    # print(program)
-    return intcode(program,1)[0]
+    whites = []
+    painted = set([])
+    rob_pos = [0,0]
+    nesw = deque([[0,1],[1,0],[0,-1],[-1,0]])
+    program = parse(inputlist)
+    int_m=intcode(program,[0])
+    a = 0
+    while int_m.program[int_m.pos] != 99:
+        get_to_output(int_m)
+        if int_m.program[int_m.pos] == 99:
+            return len(painted)
+        # print(a,nesw,int_m.mem)
+        if int_m.mem[0] == 0 and rob_pos in whites:
+            whites.remove(rob_pos)
+        if int_m.mem[0] == 1 and rob_pos not in whites:
+            whites.append(rob_pos)
+        if int_m.mem[0] == 1:
+            painted.add(tuple(rob_pos))
+        if int_m.mem[1] == 1:
+            nesw.rotate(1)
+        if int_m.mem[1] == 0:
+            nesw.rotate(-1)
+        rob_pos = [rob_pos[0]+nesw[0][0],rob_pos[1]+nesw[0][1]]
+        if rob_pos in whites:
+            int_m.inputdata=[1]
+        else:
+            int_m.inputdata=[0]
+        int_m.paramters()
+        # print(a,nesw,int_m.oc,int_m.pos)
+        # print(int_m.program[int_m.pos:int_m.pos+4],int_m.mem,int_m.inputdata)
+        # print(a,rob_pos,nesw[0],int_m.mem,whites)
+        int_m.mem = []
+        a = a+1
+        
+    return len(painted)
 
 
 
-def part2(inputlist):
-    program = parse(startlist)
-    # print(program)
-    return intcode(program,2)[0]
+def part2(inputlist):    
+    whites = [[0,0]]
+    painted = set([])
+    rob_pos = [0,0]
+    nesw = deque([[0,1],[1,0],[0,-1],[-1,0]])
+    program = parse(inputlist)
+    int_m=intcode(program,[1])
+    a = 0
+    while int_m.program[int_m.pos] != 99:
+        get_to_output(int_m)
+        if int_m.program[int_m.pos] == 99:
+            break
+        # print(a,nesw,int_m.mem)
+        if int_m.mem[0] == 0 and rob_pos in whites:
+            whites.remove(rob_pos)
+        if int_m.mem[0] == 1 and rob_pos not in whites:
+            whites.append(rob_pos)
+        if int_m.mem[0] == 1:
+            painted.add(tuple(rob_pos))
+        if int_m.mem[1] == 1:
+            nesw.rotate(1)
+        if int_m.mem[1] == 0:
+            nesw.rotate(-1)
+        rob_pos = [rob_pos[0]+nesw[0][0],rob_pos[1]+nesw[0][1]]
+        if rob_pos in whites:
+            int_m.inputdata=[1]
+        else:
+            int_m.inputdata=[0]
+        int_m.paramters()
+        # print(a,nesw,int_m.oc,int_m.pos)
+        # print(int_m.program[int_m.pos:int_m.pos+4],int_m.mem,int_m.inputdata)
+        # print(a,rob_pos,nesw[0],int_m.mem,whites)
+        int_m.mem = []
+        a = a+1
+    # print(whites)
+    x_min=99999
+    x_max=-99999
+    y_min=99999
+    y_max=-99999
+    for pos in whites:
+        x_min = min(x_min,pos[0])
+        x_max = max(x_max,pos[0])
+        y_min = min(y_min,pos[1])
+        y_max = max(y_max,pos[1])
+    print(x_min,x_max,y_min,y_max)
+    board=[' '*(x_max-x_min)]*(y_max-y_min+1)
+    for pos in whites:
+        x=pos[0]-x_min
+        y=pos[1]-y_min
+        print(x,y)
+        if pos[0] == x_max:
+            board[y] = board[y][:-1]+'#'
+        elif pos[0] == x_min:
+            board[y] = '#'+board[y][1:]
+        else:
+            board[y] = board[y][:x]+'#'+board[y][x+1:]
+
+    print(board)
+    return board
 
 #%%
