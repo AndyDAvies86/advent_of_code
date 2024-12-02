cfda1e2c4edf5cb95148abda53a1eaf05b2254af
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2024-12-02 06:26:31 +0000

    2024 day 2

--- 2024/Day02/Solution.py
+++ 2024/Day02/Solution.py
@@ -22,21 +22,51 @@ file = open("inputtest.txt","r")
 startb = file.read()
 testlist = startb.split("\n")
 
-file = open("inputtest2.txt","r")
-startc = file.read()
-test2list = startc.split("\n")
 
 #%%
 
 def parselist(inputlist):
-    pass
+    return [[int(x) for x in row.split(" ")] for row in inputlist]
 
+
+def testsafe(rep):
+    changes = [rep[ii]-rep[ii-1] for ii in range(1,len(rep))]
+    if min(changes) >= -3 and max(changes) <= -1:
+        return 1
+    elif min(changes) >= 1 and max(changes) <= 3:
+        return 1
+    return 0
 #%%
 
 def part1(inputlist):
-    pass
+    reports = parselist(inputlist)
+    safe = 0
+    for rep in reports:
+        safe = safe+testsafe(rep)
+        # print(rep,changes,safe)
+    return safe
 #%%
 
+def removes(rep):
+    if testsafe(rep[1:]) == 1:
+        return 1
+    elif testsafe(rep[:-1]) == 1:
+        return 1
+    for ii in range(1,len(rep)-1):
+        if testsafe(rep[:ii]+rep[ii+1:]) == 1:
+            return 1
+    return 0
+
+
+
 def part2(inputlist):
-    pass
+    reports = parselist(inputlist)
+    safe = 0
+    for rep in reports:
+        if testsafe(rep) == 1:
+            safe = safe+1
+        else:
+            safe = safe+removes(rep)
+
+    return safe
 #%%
