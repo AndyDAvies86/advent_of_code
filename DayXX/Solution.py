1820219bdac4cc85694e9911a40ab2d2b1084690
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2024-12-02 06:30:15 +0000

    2024 day 3 setup and template day

---
+++ DayXX/Solution.py
@@ -0,0 +1,38 @@
+
+#%%
+import pandas as pd
+import numpy as np
+import re
+import datetime as dt
+from collections import deque
+import itertools as it
+import copy
+
+#%%
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphaup = alphabet.upper()
+numbers = ['zero','one','two','three','four','five','six','seven','eight','nine']
+numlist = [str(x) for x in range(0,10)]
+#%%
+file = open("inputfile.txt","r")
+start = file.read()
+startlist = start.split("\n")
+
+file = open("inputtest.txt","r")
+startb = file.read()
+testlist = startb.split("\n")
+
+
+#%%
+
+def parselist(inputlist):
+    pass
+#%%
+
+def part1(inputlist):
+    pass
+#%%
+
+def part2(inputlist):
+    pass
+#%%
