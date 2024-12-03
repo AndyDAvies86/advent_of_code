4e287facfc08f992e36470b20c20db3df39fbf37
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2024-12-03 09:13:30 +0000

    2024 Day 3

--- 2024/Day03/Solution.py
+++ 2024/Day03/Solution.py
@@ -26,13 +26,45 @@ testlist = startb.split("\n")
 #%%
 
 def parselist(inputlist):
-    pass
+    oneline = ''.join(inputlist)
+    return oneline
 #%%
 
 def part1(inputlist):
-    pass
+    inst = parselist(inputlist)
+    muls = re.findall(r"mul\([0-9]+\,[0-9]+\)",inst)
+    score = 0
+    for mul in muls:
+        newmul = mul.replace("mul(","").replace(")","").split(",")
+        score = score + int(newmul[0])*int(newmul[1])
+    return score
 #%%
 
+def p2parse(inputlist):
+    full = ''.join(inputlist)
+    rem = copy.deepcopy(full)
+    # ind = 1
+    cleaned = ''
+    while len(rem) > 0:
+        pos = rem.find("don't()")
+        if pos == -1:
+            cleaned = cleaned + rem
+        else:
+            cleaned = cleaned + rem[:pos]
+        rem = rem[pos:]
+        posb = rem.find("do()")
+        if posb == -1:
+            rem = ''
+        else:
+            rem = rem[posb:]
+    return cleaned
+
 def part2(inputlist):
-    pass
+    inst = p2parse(inputlist)
+    muls = re.findall(r"mul\([0-9]+\,[0-9]+\)",inst)
+    score = 0
+    for mul in muls:
+        newmul = mul.replace("mul(","").replace(")","").split(",")
+        score = score + int(newmul[0])*int(newmul[1])
+    return score
 #%%
