1900cd8ecfb973634b7342df2d35fc5a50d03eb1
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2024-12-04 13:05:18 +0000

    2024 day 4

--- 2024/Day04/Solution.py
+++ 2024/Day04/Solution.py
@@ -28,11 +28,71 @@ testlist = startb.split("\n")
 def parselist(inputlist):
     pass
 #%%
+def findx(inputlist):
+    xlist = []
+    for jj in range(0,len(inputlist)):
+        for ii in range(0,len(inputlist[jj])):
+            if inputlist[jj][ii] == 'X':
+                xlist.append(np.array([ii,jj]))
+    return xlist
+
+def findwords(xlist):
+    directions = []
+    for ii in range(-1,2):
+        for jj in range(-1,2):
+            if ii != 0 or jj != 0:
+                directions.append(np.array([ii,jj]))
+    words = []
+    for x in xlist:
+        for point in directions:
+            words.append([x + d*point for d in range(0,4)])
+    return words
+
+def wordcleanse(inputlsit,words):
+    new_words = []
+    h = len(inputlsit)
+    w = len(inputlsit[0])
+    for word in words:
+        vert = [0<= word[y][1] < h for y in range(0,4)] 
+        horiz = [0<= word[y][0] < w for y in range(0,4)] 
+        if sum(vert+horiz) == 8:
+            new_words.append(word)
+    return new_words
+
+
 
 def part1(inputlist):
-    pass
+    xlist = findx(inputlist)
+    allwords = findwords(xlist)
+    words = wordcleanse(inputlist,allwords)
+    counter = 0
+    for word in words:
+        xmas = ''
+        for char in word:
+            xmas = xmas+inputlist[char[1]][char[0]]
+        if xmas == 'XMAS':
+            counter = counter+1
+    return counter
 #%%
 
+def finda(inputlist):
+    alist = []
+    for jj in range(1,len(inputlist)-1):
+        for ii in range(1,len(inputlist[jj])-1):
+            if inputlist[jj][ii] == 'A':
+                alist.append([ii,jj])
+    return alist
+
 def part2(inputlist):
-    pass
+    alist = finda(inputlist)
+    counter = 0
+    for a in alist:
+        word1 = ''
+        word2 = ''
+        for ii in range(-1,2):
+            word1 = word1+inputlist[a[1]+ii][a[0]+ii]
+            word2 = word2+inputlist[a[1]+ii][a[0]-ii]
+        if word1 in ['SAM','MAS'] and word2 in ['SAM','MAS']:
+            counter = counter+1
+    return counter
 #%%
