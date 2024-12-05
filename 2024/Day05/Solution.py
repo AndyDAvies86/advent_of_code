732e9d6c8cb45ab24176cef5f443442f667abbea
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2024-12-05 12:17:21 +0000

    2024 day 5

--- 2024/Day05/Solution.py
+++ 2024/Day05/Solution.py
@@ -16,23 +16,91 @@ numlist = [str(x) for x in range(0,10)]
 #%%
 file = open("inputfile.txt","r")
 start = file.read()
-startlist = start.split("\n")
+startlist = start.split("\n\n")
 
 file = open("inputtest.txt","r")
 startb = file.read()
-testlist = startb.split("\n")
+testlist = startb.split("\n\n")
 
 
 #%%
 
 def parselist(inputlist):
-    pass
+    inst = [[x for x in y.split("|")] for y in inputlist[0].split("\n")]
+    updates = inputlist[1].split("\n")
+    return inst,updates
 #%%
 
+def checks(inst,update):
+    mid = len(update)//2
+    midpage = int(update[mid-1:mid+1])
+    for check in inst:
+        p1 = update.find(check[0])
+        p2 = update.find(check[1])
+        if p1 != -1 and p2 != -1 and p1>p2:
+            return 0
+    return midpage
+        
+
+
 def part1(inputlist):
-    pass
+    inst,updates = parselist(inputlist)
+    score = 0
+    for update in updates:
+        # print(score)
+        score = score+checks(inst,update)
+    return score
+        
+
 #%%
 
 def part2(inputlist):
-    pass
+    inst,old_updates = parselist(inputlist)
+    updates = []
+    for upd in old_updates:
+        # print(upd,checks(inst,upd))
+        if checks(inst,upd) == 0:
+            updates.append(upd)
+    # print("endcheck")
+    # print(updates)
+    # print("clean")
+    new_updates=[]
+    score = 0
+    for jj in range(0,len(updates)):
+        update = updates[jj]
+        ii = 0
+        toclear = len(inst)
+        # print('start',jj,update)
+        while toclear > 0:
+            # print(ii,toclear)
+            check = inst[ii]
+            p1 = update.find(check[0])
+            p2 = update.find(check[1])
+            # print(p1,p2)
+            # if p1 != -1 and p2 != -1:
+
+            #     print(check,p1,p2,update,toclear)
+            if p1 != -1 and p2 != -1 and p1>p2:
+                if p1+3 < len(update):
+                    update = update[:p2]+update[p1:p1+3]+update[p2:p1]+update[p1+3:]
+                    toclear = len(inst)
+                else:
+                    update = update[:p2]+update[p1:]+','+update[p2:p1-1]
+                    toclear = len(inst)
+            else:
+                toclear = toclear - 1
+            if ii == len(inst)-1:
+                ii = -1
+            ii = ii+1
+            # print(ii)
+        new_updates.append(update)
+        mid = len(update)//2
+        midpage = int(update[mid-1:mid+1])
+        # print('score',jj,score,mid,midpage)
+        score = score+midpage
+    # print(new_updates)
+    return score
+    
+
+        
 #%%
