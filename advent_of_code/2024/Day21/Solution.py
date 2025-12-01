bd7fa2711c9f97573f5010445401d2a25f7fa1ef
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2025-12-01 05:58:52 +0000

    2024 day 21 and template

--- advent_of_code/2024/Day21/Solution.py
+++ advent_of_code/2024/Day21/Solution.py
@@ -1,16 +1,20 @@
 #%%
+import pandas as pd
+import numpy as np
+import re
+import datetime as dt
+from collections import deque,Counter
+import itertools as it
+import copy
+import networkx as nx
+import matplotlib.pyplot as plt
+from functools import lru_cache,cache
 
-
+from advent_of_code.utils.helper_functions import *
 
 #%%
-file = open("inputfile.txt","r")
-start = file.read()
-startlist = start.split("\n")
-
-file = open("inputtest.txt","r")
-startb = file.read()
-testlist = startb.split("\n")
-
+start = lineparse('inputfile.txt')
+test = lineparse('inputtest.txt')
 
 #%%
 
@@ -18,8 +22,74 @@ def parselist(inputlist):
     pass
 #%%
 
+def numpad():
+    nums = {str(x):((x+2)//3,(x+2)%3) for x in range(1,10)}
+    nums['0'] = (0,1)
+    nums['A'] = (0,2)
+    return nums
+
+def dirpad():
+    dirs = {
+        '^':(1,1),
+        '>':(0,2),
+        'v':(0,1),
+        '<':(0,0),
+        'A':(1,2)
+    }
+    return dirs
+
+@lru_cache
+def movedir(x,y,type='d'):
+    # if x==y:
+    #     return 'A'
+    if type == 'n':
+        pad = numpad()
+    else:
+        pad = dirpad()
+    xpos = pad[x]
+    ypos = pad[y]
+    out = ''   
+    if xpos[1] < ypos[1]:
+        out = out+(ypos[1]-xpos[1])*'>'
+    if xpos[0] < ypos[0]:
+        out = out+(ypos[0]-xpos[0])*'^'
+    if xpos[0] > ypos[0]:
+        out = out+(xpos[0]-ypos[0])*'v'
+    if xpos[1] > ypos[1]:
+        out = out+(xpos[1]-ypos[1])*'<'
+    return out
+
+@lru_cache
+def nextstep(intuple,p='d'):
+    tstring = tuple('A')+intuple
+    keyslist = []
+    for ii in range(0,len(intuple)):
+        newkey = movedir(tstring[ii],tstring[ii+1],p)
+        keyslist.append(newkey)
+    return keyslist
+
+
+
+def keydistance(pad):
+    dist ={
+        x+y:md(pad[x],pad[y])+1 for x in pad for y in pad
+    }
+    return dist
+
 def part1(inputlist):
-    pass
+    for code in inputlist:
+        digits = nextstep(tuple(code),'n')
+        print(code,digits)
+
+        for key in digits:
+            print(key,nextstep(key),nextstep(key[::-1]))
+
+    return
+
+    # return sum([presses[x]*int(inputlist[x][:3]) for x in range(len(inputlist))])
+
+     
+
 #%%
 
 def part2(inputlist):
