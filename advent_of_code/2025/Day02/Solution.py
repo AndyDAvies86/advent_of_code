b33d6cf54d2524a50e298af0b2e07873d2288819
AndyDAvies86 <34302986+AndyDAvies86@users.noreply.github.com>
2025-12-01 08:54:52 +0000

    day2 template and gitignore

---
+++ advent_of_code/2025/Day02/Solution.py
@@ -0,0 +1,31 @@
+#%%
+import pandas as pd
+import numpy as np
+import re
+import datetime as dt
+from collections import deque,Counter
+import itertools as it
+import copy
+import networkx as nx
+import matplotlib.pyplot as plt
+from functools import cache
+
+from advent_of_code.utils.helper_functions import *
+
+#%%
+start = lineparse('inputfile.txt')
+test = lineparse('inputtest.txt')
+
+#%%
+
+def parselist(inputlist):
+    pass
+#%%
+
+def part1(inputlist):
+    pass
+#%%
+
+def part2(inputlist):
+    pass
+#%%
